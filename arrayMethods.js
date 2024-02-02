307d142591528d3762bb94fb7bf74c92f02b782c
Sharafudeen jafani Sharafudeen <esjafani@gmail.com>
2024-02-02 06:59:40 +0530

    every

---
+++ arrayMethods.js
@@ -0,0 +1,66 @@
+
+const arraya=["Sharafu","Muhammed","Bithun","Ashwanth","Arun"];
+const arrayb=[1,2,3,4,5,6,7,8,9];
+const arrayc=[25,55,67,89,76];
+
+// Q1  concat
+    let concat=arraya.concat(arrayb);
+    document.write(concat)
+    console.log("concat result is =",concat)
+// Q2  every
+ 
+ function check(element){
+    return element<=40;
+ }
+  function evry(){
+    let every= arrayc.every(check)
+    console.log(every)
+
+  }
+ evry(arrayc)
+ 
+   
+
+// Q3  fill
+
+// Q4  find
+
+// Q5  findIndex
+
+// Q6  flat
+
+// Q7  forEach
+
+// Q8  includes
+
+// Q9  indexOf
+
+
+// Q12 pop
+
+// Q13 push
+
+// Q14 Reverse
+
+// Q15 unshift
+
+// Q16 shift
+
+// Q17 slice
+
+// Q18 some
+
+// Q19 sort
+
+
+// Q20 splice
+
+// Q21 toString
+
+// Q22 Filter
+
+// Q23 Reduce
+
+// Q24 Map
+
+// Q25 forEach
---
+++ stringMethod.js
@@ -0,0 +1,50 @@
+
+// Q1  concat
+
+// Q2  endsWith
+
+// Q3  includes
+
+// Q4  indexOf
+
+// Q5  lastIndexOf
+
+
+// Q6  match
+
+// Q7  matchAll
+
+// Q8  padEnd
+
+// Q9  padStart
+
+// Q10 repeat
+
+// Q11 replace
+
+// Q12 search
+
+// Q13 slice
+
+// Q14 split
+
+// Q15 startsWith
+
+
+// Q16 substr
+
+// Q17 substring
+
+// Q18 toLowerCase
+
+// Q19 toUpperCase
+
+// Q20 trim
+
+// Q21 trimEnd (or trimRight)
+
+// Q22 trimStart (or trimLeft)
+
+// Q23 charAt
+
+// Q24 charCodeAt
